c4bf5c4e7021014e870c54a36af7826840567e4e
Samilly <samillynunes19@gmail.com>
2025-02-17 17:40:46 -0300

    feat: GlobalStyles and GradientBackground components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+
+const GradientBackground = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  width: 100%;
+  min-height: 100vh;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    
-    </>
-  )
+  return <GradientBackground>
+    <GlobalStyles></GlobalStyles>
+  </GradientBackground>;
 }
 
-export default App
+export default App;
