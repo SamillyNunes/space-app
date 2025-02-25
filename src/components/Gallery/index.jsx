9a0a9bfa706881a5b03507ca74b3fa2de90a3565
Samilly <samillynunes19@gmail.com>
2025-02-24 21:07:44 -0300

    chore: working on gallery content

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,6 +1,28 @@
+import styled from "styled-components";
+import Title from "../Title";
+import Popular from "./Popular";
+import Tags from "./Tags";
+
+const GalleryContainer = styled.div`
+    display: flex;
+`;
+
+const FluidSection = styled.section`
+    flex-grow: 1;
+`;
+
 const Gallery = () => {
     return (
-        <h2>Navegue pela galeria</h2>
+        <>
+            <Tags />
+
+            <GalleryContainer>
+                <FluidSection>
+                    <Title>Navegue pela galeria</Title>
+                </FluidSection>
+                <Popular/>
+            </GalleryContainer>
+        </>
     );
 }
 
---
+++ src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h2`
+    color: #7B78E6;
+    font-size: 32px;
+    font-weight: 400;
+    text-align: ${props => props.$alignment ? props.$alignment : 'left'};
+
+`;
+
+export default Title;
