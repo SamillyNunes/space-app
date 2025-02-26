a6daf22c7b2e62cada565d94788ce57dbe5ced1b
Samilly <samillynunes19@gmail.com>
2025-02-26 07:47:01 -0300

    chore: added key to photos rendering

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -5,36 +5,38 @@ import Tags from "./Tags";
 import Image from "./Image";
 
 const GalleryContainer = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const FluidSection = styled.section`
-    flex-grow: 1;
+  flex-grow: 1;
 
-    & > div{
-        display: flex;
-        flex-wrap: wrap;
-        gap: 1.15rem;
-        row-gap: 0;
-    }
+  & > div {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1.15rem;
+    row-gap: 0;
+  }
 `;
 
-const Gallery = ({photos = []}) => {
-    return (
-        <>
-            <Tags />
+const Gallery = ({ photos = [] }) => {
+  return (
+    <>
+      <Tags />
 
-            <GalleryContainer>
-                <FluidSection>
-                    <Title>Navegue pela galeria</Title>
-                    <div>
-                    {photos.map(p => <Image photo={p} />)}
-                    </div>
-                </FluidSection>
-                <Popular/>
-            </GalleryContainer>
-        </>
-    );
-}
+      <GalleryContainer>
+        <FluidSection>
+          <Title>Navegue pela galeria</Title>
+          <div>
+            {photos.map((p) => (
+              <Image key={p.id} photo={p} />
+            ))}
+          </div>
+        </FluidSection>
+        <Popular />
+      </GalleryContainer>
+    </>
+  );
+};
 
 export default Gallery;
